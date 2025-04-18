a15cc506b53979b8906603fc47354af0cf704462
Lakshya <89395485+joshilakshya@users.noreply.github.com>
2025-04-19 01:15:28 +0530

    Create StudentAlreadyExistsException.java

---
+++ StudentAlreadyExistsException.java
@@ -0,0 +1,7 @@
+//StudentAlreadyExistsException.java
+
+public class StudentAlreadyExistsException extends Exception {
+    public StudentAlreadyExistsException(String message) {
+        super(message);
+    }
+}
